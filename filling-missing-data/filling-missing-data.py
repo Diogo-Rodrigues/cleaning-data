be80d2cee1cbf892ca0148b5f91bb89d4d274c40
Diogo-Rodrigues <diogorodrigues0801@gmail.com>
2018-08-24 00:24:49 -0300

    Create a python code that filling missing datas from a data set using pandas.

---
+++ filling-missing-data/filling-missing-data.py
@@ -0,0 +1,14 @@
+# import pandas as pd
+import pandas as pd
+
+# read csv file
+airquality = pd.read_csv('../datasets/airquality.csv')
+
+# Calculate the mean of the Ozone column: oz_mean
+oz_mean = airquality.Ozone.mean()
+
+# Replace all the missing values in the Ozone column with the mean
+airquality['Ozone'] = airquality.Ozone.fillna(oz_mean)
+
+# Print the info of airquality
+print(airquality.info())
